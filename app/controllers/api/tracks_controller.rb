f310589103a631636590bebdf22e765ac39026ee
Sean O'Dubhghaill <jdubhghaill@gmail.com>
2015-08-28 15:38:33 +1000

    throw a few headers into the mix

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,6 +1,9 @@
 module Api
   class  TracksController < Api::BaseController
     def data
+        response.headers['X-Content-Duration'] = '311'
+        response.headers['Content-Type'] = 'audio/mpeg'
+        response.headers['Accept-Ranges'] = 'bytes'
 	send_file '/home/sean/Music/mirah - 01 - cold cold water.ogg'
     end
 
